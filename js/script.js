57f1ff004aceb8c2c834d3fd89f9f8faa22a352d
M M Aminur Jibon <38575926+aminurjibon0@users.noreply.github.com>
2019-09-24 08:13:47 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function candleOn() {
-    document.getElementById('candle').src = '../images/candle-on.jpg';
+    document.getElementById('candle').src = 'images/candle-on.jpg';
 }
 function candleOff() {
-    document.getElementById('candle').src = '../images/candle-off.jpg';
+    document.getElementById('candle').src = 'images/candle-off.jpg';
 }
